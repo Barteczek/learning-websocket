592d4ffe6da85ba89f2a40eee901020de287e74e
Bartosz Łepski <lepski.bartosz@gmail.com>
2020-08-12 23:05:12 +0200

    Implement socket to develop chat funcionality

--- client/app.js
+++ client/app.js
@@ -5,29 +5,52 @@ const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
+const socket = io();
+
 let userName = '';
+
+socket.on('message', ({ author, content }) => addMessage(author, content))
+socket.on('newUser', (name) => addMessage('Chat Bot', name))
 
 const login = (event) => {
   event.preventDefault();
-  !userNameInput.value ? alert('Please provide user name') : userName = userNameInput.value;
-  loginForm.classList.remove('show');
-  messagesSection.classList.add('show');
+  if(!userNameInput.value) {
+    alert('Please provide user name')
+  } else {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+    socket.emit('login', { name: userName })
+  }
 }
 
 loginForm.addEventListener('submit', login);
 
 const sendMessage = (event) => {
   event.preventDefault();
-  !messageContentInput.value ? alert('You can\'t send empty message') : addMessage(userName, messageContentInput.value);
-  messageContentInput.value = '';
+  let messageContent = messageContentInput.value;
+
+  if(!messageContent.length) {
+    alert('You have to type something!');
+  }
+  else {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent })
+    messageContentInput.value = '';
+  }
 }
 
 const addMessage = (author, content) => {
   const message = document.createElement('li');
   message.setAttribute('class', 'message message--received');
-  author === userName ? message.classList.add('message--self') : null;
+  if(author === userName) {
+    message.classList.add('message--self')
+  } else if(author === 'Chat Bot') {
+    message.classList.add('message--bot')
+  }
+
   message.innerHTML = `
-    <h3 class="message__author">${author === userName ? 'You' : userName}</h3>
+    <h3 class="message__author">${author === userName ? 'You' : author}</h3>
     <div class="message__content">
       ${content}
     </div>
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const socket = require('socket.io');
 
 const messages = [];
+const users = [];
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client')));
@@ -11,6 +13,43 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000)
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  
+  socket.on('login', ({name}) => {
+    const newUser = {
+      name: name,
+      id: socket.id
+    }
+    users.push(newUser)
+    socket.broadcast.emit('message', {
+      author: 'Chat Bot',
+      content: `${name} has joined the conversation!`,
+    });
+  });
+
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => { 
+    console.log('Oh, socket ' + socket.id + ' has left') 
+    for(i = 0; i < users.length; i++) {
+      if(users[i].id === socket.id) {
+        socket.broadcast.emit('message', {
+          author: 'Chat Bot',
+          content: `${users[i].name} has left the conversation...`,
+        });
+        users.splice(i, 1);
+      }
+    }
+  });
+  console.log('I\'ve added a listener on message event \n');
 });
